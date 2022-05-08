7028e836faa537c964361fbc3d500c2d73cd2186
Yash Mittal <23.yashm@gmail.com>
2022-05-08 19:06:13 +0530

    Basic Multilingual

---
+++ main/locale.py
@@ -0,0 +1,18 @@
+trans = {
+  "Subscribe to our mails":["Subscribe to our mails" , "हमारे मेल की सदस्यता लें"],
+  "Welcome to Sahayata Portal":["Welcome to Sahayata Portal","सहायता पोर्टल में आपका स्वागत है"],
+  "Click on the appropriate category:":["Click on the appropriate category:","उपयुक्त श्रेणी पर क्लिक करें:"],
+  "Students":["Students","छात्र"],
+  "Labourers":["Labourers","मजदूरों"],
+  "Employment Schemes":["Employment Schemes","रोजगार योजनाएं"],
+  "Social Welfare Schemes":["Social Welfare Schemes","समाज कल्याण योजनाएं"],
+  "Women":["Women","औरत"],
+  "Subscribe for all latest updates!":["Subscribe for all latest updates!","सभी नवीनतम अपडेट के लिए सदस्यता लें!"],
+  "Language":["Language","भाषा"],
+  "Our Team":["Our Team","हमारी टीम"],
+  "About":["About","हमारे बारे में"],
+  "Home":["Home","घर"],
+  "National Scholarships":["National Scholarships","राष्ट्रीय छात्रवृत्ति"],
+  "International Scholarships":["International Scholarships","अंतर्राष्ट्रीय छात्रवृत्ति"],
+  "All Rights Reserved © CP301_G1 2022":["All Rights Reserved © CP301_G1 2022","सर्वाधिकार सुरक्षित © CP301_G1 2022"]
+}
--- main/templatetags/filters.py
+++ main/templatetags/filters.py
@@ -1,7 +1,19 @@
 from django import template
+from main.locale import trans
 
 register = template.Library()
 
 @register.simple_tag
 def print_date(dt):
   return dt.strftime("%d-%b-%Y")
+
+@register.simple_tag
+def translate(request, text):
+  lan = request.COOKIES.get('lan','en') 
+  try:
+    if lan=="en":
+      return trans[text][0]
+    elif lan=="hi":
+      return trans[text][1]
+  except:
+    return text
